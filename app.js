b7db9b650841d1a62c1aa72b0224d457a61ef5d5
Ayush <ayush4002gupta@gmail.com>
2022-07-16 18:38:48 +0530

    ecomd commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const mongoose = require("mongoose");
+
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+mongoose.connect("mongodb+srv://Ayush:ayushishere@cluster0.fohsg.mongodb.net/PortfolioDatabase", {useNewUrlParser: true});
+const Contact = {
+
+    Name: String, 
+    Subject: String,
+    Message: String
+   
+   };
+   const Contactmodel = mongoose.model("Contactcollection", Contact);
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+app.post("/contact",function(req,res){
+    const Post = new Contactmodel({
+        Name: req.body.name, 
+        Subject: req.body.subject,
+        Message: req.body.message
+    });
+  
+ 
+    Post.save(function(err){
+
+        if (!err){
+     
+
+          
+        console.log("hi");
+     
+
+     
+        };
+
+      });
+  
+})
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
